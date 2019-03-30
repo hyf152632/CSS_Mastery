c1496d2683fa75c2c676949413f53375471129c0
yongfei huo <hyfpersonal@outlook.com>
2019-03-30 22:17:24 +0800

    add content

--- src/index.js
+++ src/index.js
@@ -6,5 +6,4 @@ document.getElementById("app").innerHTML = `
   We use Parcel to bundle this sandbox, you can find more info about Parcel
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
-<p>jj</p>
 `;
